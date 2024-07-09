ea7d4fe68d9ab58fae81db746c7bd5ac86642952
LutchenkoOleh <160421286+LutchenkoOleh@users.noreply.github.com>
2024-07-09 17:52:09 +0200

    two-try

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -8,21 +8,24 @@ const input = document.querySelector('.input')
 const container = document.querySelector('.gallery')
 
 
+const API_KEY = '44806225-40e07737f22f709bd193bb0f7';
+const URL = `https://pixabay.com/api/?key=${API_KEY}`;
+const imageType = 'photo';
+const orientation = 'horizontal';
+const safeSearch = true;
+
+
 function searchImg(query) {
 
-  const API_KEY = '44806225-40e07737f22f709bd193bb0f7';
-  const URL = `https://pixabay.com/api/?key=${API_KEY}`;
-  const imageType = 'photo';
-  const orientation = 'horizontal';
-  const safeSearch = true;
 
-  return fetch(`${URL}&q=${query}%image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`)
+  return fetch(`${URL}&q=${query}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`)
     .then((res) => {
       if (!res.ok) {
         throw new Error('no match item')
       }
       return res.json()
     })
+
 
 }
 
@@ -44,101 +47,74 @@ function handelSearch(e) {
   }
 
   searchImg(queryValue)
-    .then(renderImg)
-    .catch(onFetchError)
+    .then((data) => {
+      const results = data.hits;
+      const totalRes = data.totalHits;
+      if (totalRes === 0) {
+        iziToast.error({
+          backgroundColor: '#ef4040',
+          messageColor: '#fff',
+          messageSize: '16px',
+          position: 'topRight',
+          message: 'Sorry, there are no images matching your search query. Please try again!'
+        })
+      } else {
+        results.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+          const markup = `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}"></a>
+      <div class="overlay">
+        <img src="${webformatURL}" alt="${tags}" class="gallery-img">
+        <div class="text-wrap">
+          <p class="img-text">Likes: <span>${likes}</span></p>
+          <p class="img-text">Views: <span>${views}</span></p>
+          <p class="img-text">Comments: <span>${comments}</span></p>
+          <p class="img-text">Downloads: <span>${downloads}</span></p>
+        </div>
+      </div>
+    </li>`
 
+          container.insertAdjacentHTML('afterbegin', markup);
+
+        })
+
+        let galleryShow = new SimpleLightbox('.gallery a', {
+          captions: true,
+          captionType: 'attr',
+          captionsData: 'alt',
+          captionPosition: 'bottom',
+          captionDelay: 250,
+        });
+        galleryShow.on('show.simplelightbox', function () {
+
+        });
+
+      }
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
-function onFetchError(error) {
-  iziToast.error({
-    backgroundColor: '#ef4040',
-    messageColor: '#fff',
-    messageSize: '16px',
-    position: 'topRight',
-    message: 'Sorry, there are no images matching your search query. Please try again!'
-  })
-}
 
-function renderImg({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-  searchImg()
-    .then(data => console.log(data.hits))
-    .catch(err => console.error(err))
 
-  const markup = `
-  <li class="item">
-  <a class="gallery-link" href="${largeImageURL}">
-   <img src="${webformatURL}" alt="${tags}" class="img-class">
-   <p class="img-text">Likes:${likes}</p>
-   <p class="img-text">Views:${views}</p>
-   <p class="img-text">Comments:${comments}</p>
-   <p class="img-text">Downloads:${downloads}</p>
-  </li>`
+// function renderImg({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
 
-  container.innerHTML = markup;
-}
+//   const markup = `
+//   <ul class="list-img">
+//    <li class="item">
+//     <a class="gallery-link" href="${largeImageURL}">
+//     <img src="${webformatURL}" alt="${tags}" class="img-class">
+//     <p class="img-text">Likes:${likes}</p>
+//     <p class="img-text">Views:${views}</p>
+//     <p class="img-text">Comments:${comments}</p>
+//     <p class="img-text">Downloads:${downloads}</p>
+//    </li>
+//   </ul>`
 
-// function createMarkup(hits) {
-//   return hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-//     `
-//   <li class="item">
-//   <a class="gallery-link" href="${largeImageURL}">
-//    <img src="${webformatURL}" alt="${tags}" class="img-class">
-//    <p class="img-text">Likes:${likes}</p>
-//    <p class="img-text">Views:${views}</p>
-//    <p class="img-text">Comments:${comments}</p>
-//    <p class="img-text">Downloads:${downloads}</p>
-//   </li>`).join("")
+//   container.innerHTML = markup;
 // }
 
 
 
-
-// searchBtn.addEventListener('submit', handlerFormSubmit)
-
-// form.addEventListener('submit', handlerFormSubmit)
-
-// function handlerFormSubmit(e) {
-//   e.preventDefault();
-
-//   const formTarget = e.currentTarget;
-//   const queryValue = formTarget.elements.query.value.toLowerCase();
-
-
-
-//   const API_KEY = '44806225-40e07737f22f709bd193bb0f7';
-//   const URL = `https://pixabay.com/api/?key=${API_KEY}`;
-//   const q = queryValue;
-//   const imageType = 'photo';
-//   const orientation = 'horizontal';
-//   const safeSearch = true;
-
-//   const apiUrl = `${URL}&q=${q}%image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`;
-
-
-//   function getObject(query) {
-//     return fetch(apiUrl)
-//       .then((res) => {
-//         if (!res.ok) {
-//           throw new Error(res.status)
-//         }
-
-//         return res.json
-//       })
-//   }
-
-//   getObject(queryValue)
-//     .then(data => {
-//       if (data = []) {
-//         iziToast.error({
-//           backgroundColor: '#ef4040',
-//           messageColor: '#fff',
-//           messageSize: '16px',
-//           position: 'topRight',
-//           message: 'Sorry, there are no images matching your search query. Please try again!'
-//         })
-//       } else {
-//         console.log(data.hits);
-//       }
-//     })
-//     .catch(err => console.error(err))
-// }
